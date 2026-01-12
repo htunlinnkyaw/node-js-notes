f28f214c1a594c279e9a5086cb75f5934e4945e6
htunlinnkyaw <tunlinkyaw4781@gmail.com>
2026-01-12 15:24:32 +0630

    notes: response file

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
-const express = require('express');
-const app = express();
+const express = require('express'),
+     app = express(),
+     path = require('path');
+
+app.get('/', (req, res) => res.send('Hello World!'));
 
 app.get('/names',(req,res)=> {
+    res.sendFile(path.join(__dirname,'/index.html'));
     // res.send('Hello World!');
     // res.json({names:["shaun","john","lucifer","blah blah"]})
 })
 
-app.listen(3000,()=> console.log(`Server running on port 3000`));
+app.listen(3000,()=> {
+    console.clear();
+    // console.log('Dir Name is ', __dirname);
+    console.log(`Server running on port 3000`);
+});
 
